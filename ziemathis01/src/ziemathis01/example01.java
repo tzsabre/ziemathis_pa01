8d3520694dd834159aa7d9d9aaf58ea35b78be38
Timothy Ziemathis <timothyziemathis@knights.ucf.edu>
2020-01-18 21:37:43 -0500

    My first commit

---
+++ ziemathis01/src/ziemathis01/example01.java
@@ -0,0 +1,16 @@
+package ziemathis01;
+
+import java.util.Scanner;
+
+public class example01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Please enter your name");
+		String name = in.next();
+		System.out.println("Hello, "+ name);
+	}
+
+}
